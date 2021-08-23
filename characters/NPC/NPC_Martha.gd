ee701c48773f29205ed4fe06ba4fcc6244d8b25b
MysteRys337 <mysteriousfunkyman@gmail.com>
2021-08-22 23:50:06 -0300

    Player/NPC Interaction -feat: Added animated speech bubble -feat: first draft of conversation mechanic(using Dialogic addon)

--- characters/NPC/NPC_Martha.gd
+++ characters/NPC/NPC_Martha.gd
@@ -4,15 +4,25 @@ var groupName = 'interactable_npc'
 onready var sprites = $AnimatedSprite
 onready var speech_bubble = $"Speech Bubble"
 
+var in_conversation : bool
+
 func _ready():
-	speech_bubble.visible = false
+	_speech_bubble_off()
+	in_conversation = false
 	
 func interact_with_player(player_x,player_y):
-	face_the_player(player_x,player_y)
+	if !in_conversation:
+		in_conversation = true
+		face_the_player(player_x,player_y)
+		var new_dialog = Dialogic.start('martha-first-conversation')
+		add_child(new_dialog)
+		new_dialog.connect("timeline_end", self, "after_dialog_end")
+	
+func after_dialog_end(timeline_name):
+	in_conversation = false
 	
 func face_the_player(player_x,player_y):
 	var curr_direction = Vector2(player_x - self.position.x, self.position.y - player_y).normalized()
-	print(curr_direction)
 	
 	if(curr_direction.y < -0.5):
 		sprites.set_frame(0)
@@ -30,9 +40,18 @@ func face_the_player(player_x,player_y):
 
 func _on_InteractionZone_area_entered(area):
 	self.add_to_group(groupName)
-	speech_bubble.visible = true
+	_speech_bubble_on()
 
 
 func _on_InteractionZone_area_exited(area):
 	self.remove_from_group(groupName)
+	_speech_bubble_off()
+
+func _speech_bubble_off():
 	speech_bubble.visible = false
+	speech_bubble.stop()
+	speech_bubble.set_frame(0)
+	
+func _speech_bubble_on():
+	speech_bubble.visible = true
+	speech_bubble.play("default", false)
